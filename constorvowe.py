9fac95a71c458dcdeb7f9075907f130ef1cd5f9f
ashwin1807 <54059830+ashwin1807@users.noreply.github.com>
2019-08-13 22:56:01 +0530

    Add files via upload

--- constorvowe.py
+++ constorvowe.py
@@ -2,7 +2,7 @@ m=str(input(" "))
 if (m=='a' or m=='e' or m=='i' or m=='o' or m=='u'):
     print("Vowel")
 elif (m=='b' or m=='c' or m=='d' or m=='f' or m=='g' or m=='h' or m=='j' or m=='k' or m=='l' or m=='m' or m=='n' or m=='p' or m=='q' or m=='r' or m=='s' or m=='t' or m=='v' or m=='w' or m=='x' or m=='y' or m=='z'):
-    print("Constant")
+    print("Consonant")
 else:
     print("Invalid")
             
