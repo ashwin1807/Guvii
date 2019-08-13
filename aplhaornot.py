ff7e40e016e0bac2abb8a0dc74bc8ea0afe4f09a
ashwin1807 <54059830+ashwin1807@users.noreply.github.com>
2019-08-13 23:02:14 +0530

    Add files via upload

---
+++ aplhaornot.py
@@ -0,0 +1,6 @@
+m=str(input(" "))
+if (m=='a' or m=='b' or m=='c' or m=='d' or m=='e' or m=='f' or m=='g' or m=='h' or m=='i' or m=='j' or m=='k' or m=='l' or m=='m' or m=='n' or m=='o' or m=='p' or m=='q' or m=='r' or m=='s' or m=='t' or m=='u' or m=='v' or m=='w' or m=='x' or m=='y' or m=='z'):
+    print("Alphabet")
+else:
+    print("No")
+            
